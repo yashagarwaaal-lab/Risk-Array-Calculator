665033c8b937e6341ec0c329446a71bfc68899f1
yashagarwaaal-lab <yashagarwaaal@gmail.com>
2026-04-28 15:20:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import math
 from scipy.stats import norm #Used for getting z values of the confidence level
 from pathlib import Path #Used for getting file paths
 from scipy.stats import genpareto
-from tkcalendar import DateEntry
 import xml.etree.ElementTree as ET
 from lxml import etree
 import pickle
